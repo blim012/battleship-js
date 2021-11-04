3af5103c9f70de95ef619e91a9a62f4b6ffdd127
Brandon Lim <brandonlimchi@gmail.com>
2021-11-03 18:56:40 -0700

    remove player index from gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,36 +1,38 @@
 const Ship = require('./ship');
 
-const Gameboard = (defaultShips = [[], []]) => {
+const Gameboard = (defaultShips = []) => {
   let ships = defaultShips;
   let missedBitBoard = 0n;
 
   const getMissedBitBoard = () => missedBitBoard;
 
-  const placeShip = (playerNum, placeBitBoard) => {
-    let playerShipsPlacement = getPlayerShipsPlacement(playerNum);
-    if(playerShipsPlacement & placeBitBoard) // illegal placement 
-      return playerShipsPlacement; 
-    ships[playerNum].push(Ship(placeBitBoard));
-    return playerShipsPlacement | placeBitBoard;
+  const placeShip = (placeBitBoard) => {
+    let shipsPlacement = getShipsPlacement();
+    if(shipsPlacement & placeBitBoard) // illegal placement 
+      return shipsPlacement; 
+    ships.push(Ship(placeBitBoard));
+    return shipsPlacement | placeBitBoard;
   };
 
-  const receiveAttack = (playerNum, attackBitBoard) => {
-    let playerShips = ships[playerNum];
-    for(let i = 0; i < playerShips.length; i++) {
-      if(playerShips[i].getPositionBitBoard() & attackBitBoard)
-        return playerShips[i].hit(attackBitBoard);
+  const receiveAttack = (attackBitBoard) => {
+    for(let i = 0; i < ships.length; i++) {
+      if(ships[i].getPositionBitBoard() & attackBitBoard)
+        return ships[i].hit(attackBitBoard);
     }
     missedBitBoard |= attackBitBoard;
     return 0n;
   };
 
-  const getPlayerShipsPlacement = (playerNum) => {
-    let playerShips = ships[playerNum];
-    if(playerShips.length === 0) return 0n;
-    return playerShips.reduce((playerShipsPlacement, ship) => { 
-      return playerShipsPlacement | ship.getPositionBitBoard() }, 0n);
+  const isAllSunk = () => {
+    
   };
-  return { getMissedBitBoard, placeShip, receiveAttack };
+
+  const getShipsPlacement = () => {
+    if(ships.length === 0) return 0n;
+    return ships.reduce((shipsPlacement, ship) => { 
+      return shipsPlacement | ship.getPositionBitBoard() }, 0n);
+  };
+  return { getMissedBitBoard, placeShip, receiveAttack, isAllSunk };
 };
 
 module.exports = Gameboard;
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -5,45 +5,55 @@ describe('placeShip function tests', () => {
   test('Places ship if placement is legal, returns updated ship placements', () =>
   {
     const gameboard = Gameboard()
-    let playerNum = 0;
     let placeBitBoard = 0xFn; // A1 -> D1, Length 4 ship
-    expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0xFn);
+    expect(gameboard.placeShip(placeBitBoard)).toBe(0xFn);
     placeBitBoard = 0xF0n; // E1 -> H1, Length 4 ship
-    expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0xFFn);
+    expect(gameboard.placeShip(placeBitBoard)).toBe(0xFFn);
     placeBitBoard = 0x400n; // A2, Length 1 ship
-    expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0x4FFn);
+    expect(gameboard.placeShip(placeBitBoard)).toBe(0x4FFn);
   });
 
   test('Does not place a ship if placement is illegal, returns non-updated ship placements', () =>{
     const gameboard = Gameboard()
-    let playerNum = 0;
     let placeBitBoard = 0xFn; // A1 -> D1, Length 4 ship
-    expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0xFn);
+    expect(gameboard.placeShip(placeBitBoard)).toBe(0xFn);
     placeBitBoard = 0x401n // A1 -> A2, Length 2 ship
-    expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0xFn);
+    expect(gameboard.placeShip(placeBitBoard)).toBe(0xFn);
   });
 });
 
 describe('receiveAttack function tests', () => {
   test('Update hit bitboard on successful hit, returns updated hitBitBoard of ship', () => {
-    const gameboard = Gameboard([[Ship(0xFn)], []]); // Player 1 has one ship, at A1 -> D1
-    let playerNum = 0;
+    const gameboard = Gameboard([Ship(0xFn)]); // Player 1 has one ship, at A1 -> D1
     let attackBitBoard = 0x1n; // Attack at A1
-    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0x1n);
+    expect(gameboard.receiveAttack(attackBitBoard)).toBe(0x1n);
     attackBitBoard = 0x2n;
-    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0x3n);
+    expect(gameboard.receiveAttack(attackBitBoard)).toBe(0x3n);
     attackBitBoard = 0x8n;
-    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0xBn);
+    expect(gameboard.receiveAttack(attackBitBoard)).toBe(0xBn);
   });
 
   test('Update missed shot bitboard on missed hit, returns 0n', () => {
-    const gameboard = Gameboard([[Ship(0xFn)], []]); // Player 1 has one ship, at A1 -> D1
-    let playerNum = 0;
+    const gameboard = Gameboard([Ship(0xFn)]); // Player 1 has one ship, at A1 -> D1
     let attackBitBoard = 0x10n; // Attack at E1
-    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0n);
+    expect(gameboard.receiveAttack(attackBitBoard)).toBe(0n);
     expect(gameboard.getMissedBitBoard()).toBe(attackBitBoard);
     attackBitBoard = 0x400n; // Attack at A2
-    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0n);
+    expect(gameboard.receiveAttack(attackBitBoard)).toBe(0n);
     expect(gameboard.getMissedBitBoard()).toBe(0x410n);
   });
 });
+
+/*
+describe('isAllSunk function tests', () =>{
+  test('returns true if all ships in gameboard have sunk', () => {
+    const gameboard = Gameboard([[Ship(0x1n), Ship(0x10n), Ship(0x100n)], []]);
+    let playerNum = 0;
+    let attackBitBoard = 0x1n;
+    gameboard.receiveAttack(playerNum, attackBitBoard);
+    gameboard.receiveAttack(playerNum, attackBitBoard << 1n);
+    gameboard.receiveAttack(playerNum, attackBitBoard << 2n);
+    expect(gameboard.isAllSunk(playerNum)).toBe(true);
+  });
+});
+*/
