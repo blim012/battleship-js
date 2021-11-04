c13f7cca15dc6b9439e1dd70e8cbec6cac8f544a
Brandon Lim <brandonlimchi@gmail.com>
2021-11-03 18:42:36 -0700

    implement receiveAttack function in Gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,10 @@
 const Ship = require('./ship');
 
-const Gameboard = () => {
-  let ships = [[], []];
+const Gameboard = (defaultShips = [[], []]) => {
+  let ships = defaultShips;
+  let missedBitBoard = 0n;
+
+  const getMissedBitBoard = () => missedBitBoard;
 
   const placeShip = (playerNum, placeBitBoard) => {
     let playerShipsPlacement = getPlayerShipsPlacement(playerNum);
@@ -11,13 +14,23 @@ const Gameboard = () => {
     return playerShipsPlacement | placeBitBoard;
   };
 
+  const receiveAttack = (playerNum, attackBitBoard) => {
+    let playerShips = ships[playerNum];
+    for(let i = 0; i < playerShips.length; i++) {
+      if(playerShips[i].getPositionBitBoard() & attackBitBoard)
+        return playerShips[i].hit(attackBitBoard);
+    }
+    missedBitBoard |= attackBitBoard;
+    return 0n;
+  };
+
   const getPlayerShipsPlacement = (playerNum) => {
     let playerShips = ships[playerNum];
     if(playerShips.length === 0) return 0n;
     return playerShips.reduce((playerShipsPlacement, ship) => { 
       return playerShipsPlacement | ship.getPositionBitBoard() }, 0n);
   };
-  return { placeShip };
+  return { getMissedBitBoard, placeShip, receiveAttack };
 };
 
 module.exports = Gameboard;
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,7 +1,8 @@
 const Gameboard = require('../src/gameboard');
+const Ship = require('../src/ship');
 
 describe('placeShip function tests', () => {
-  test('Places ship if placement is legal', () =>
+  test('Places ship if placement is legal, returns updated ship placements', () =>
   {
     const gameboard = Gameboard()
     let playerNum = 0;
@@ -13,12 +14,36 @@ describe('placeShip function tests', () => {
     expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0x4FFn);
   });
 
-  test('Does not place a ship if placement is illegal', () =>{
+  test('Does not place a ship if placement is illegal, returns non-updated ship placements', () =>{
     const gameboard = Gameboard()
     let playerNum = 0;
     let placeBitBoard = 0xFn; // A1 -> D1, Length 4 ship
     expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0xFn);
     placeBitBoard = 0x401n // A1 -> A2, Length 2 ship
     expect(gameboard.placeShip(playerNum, placeBitBoard)).toBe(0xFn);
+  });
+});
+
+describe('receiveAttack function tests', () => {
+  test('Update hit bitboard on successful hit, returns updated hitBitBoard of ship', () => {
+    const gameboard = Gameboard([[Ship(0xFn)], []]); // Player 1 has one ship, at A1 -> D1
+    let playerNum = 0;
+    let attackBitBoard = 0x1n; // Attack at A1
+    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0x1n);
+    attackBitBoard = 0x2n;
+    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0x3n);
+    attackBitBoard = 0x8n;
+    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0xBn);
+  });
+
+  test('Update missed shot bitboard on missed hit, returns 0n', () => {
+    const gameboard = Gameboard([[Ship(0xFn)], []]); // Player 1 has one ship, at A1 -> D1
+    let playerNum = 0;
+    let attackBitBoard = 0x10n; // Attack at E1
+    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0n);
+    expect(gameboard.getMissedBitBoard()).toBe(attackBitBoard);
+    attackBitBoard = 0x400n; // Attack at A2
+    expect(gameboard.receiveAttack(playerNum, attackBitBoard)).toBe(0n);
+    expect(gameboard.getMissedBitBoard()).toBe(0x410n);
   });
 });
