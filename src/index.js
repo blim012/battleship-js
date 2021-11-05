26877a43830538b63d522d4080939f309a07f9be
Brandon Lim <brandonlimchi@gmail.com>
2021-11-05 09:55:51 -0700

    initial page UI implementation

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Player from './player.js';
+import Gameboard from './gameboard.js';
+import Ship from './ship.js';
+
+import './reset.css';
+import './style.css';
+
+window.Player = Player;
+window.Gameboard = Gameboard;
+window.Ship = Ship;
