c100a0c19a99ccac0b9b94701f2b90796e930ebe
Brandon Lim <brandonlimchi@gmail.com>
2021-11-05 17:00:30 -0700

    implement bitboard to tile number array function

--- src/logic/bitboard.js
+++ src/logic/bitboard.js
@@ -4,8 +4,16 @@ const Bitboard = (() => {
     bigIntNum === 0n ? 0 : Math.log2(Number(bigIntNum & ~(bigIntNum - 1n))); 
   const tileNumToBitBoard = (tileNum) =>
     1n << BigInt((tileNum - 1));
+  const bitboardToTileNum = (bitboard) => {
+    let tileNums = [];
+    while(bitboard != 0) {
+      tileNums.push(getNumTrailingZeros(bitboard) + 1);
+      bitboard &= (bitboard - 1n);
+    }
+    return tileNums;
+  }
 
-  return { getLSB, getNumTrailingZeros, tileNumToBitBoard };
+  return { getLSB, getNumTrailingZeros, tileNumToBitBoard, bitboardToTileNum };
 })();
 
 export default Bitboard;
